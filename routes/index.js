9cf8ce59c1eab7da0df87a00e07790a852156c91
kassergey <kassergey@mail.ru>
2016-05-30 15:30:51 +0300

    Some changed

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/storeCart', function(req, res, next) {
+    res.render('storeCart', { title: 'Express' });
+});
+
 module.exports = router;
